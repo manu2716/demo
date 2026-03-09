13cdc143a5fe314fc56b2a312f081d975af11c16
Manu Yadav <manuyadav2716@gmail.com>
2026-03-09 18:22:13 +0100

    code to add swagger documentation

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-To run application from IDE
+### Run application from IDE Terminal
 mvn spring-boot:run
+### Swagger Documentation URL
+http://localhost:8080/swagger-ui.html
+### OPENAPI JSON
+http://localhost:8080/v3/api-docs
--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -2,6 +2,7 @@ package com.example.controller;
 
 import com.example.model.Product;
 import com.example.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,6 +21,10 @@ public class ProductController {
     }
 
     //TODO: Refactor GET to check for null or empty list
+    @Operation(
+            summary = "Get All Product",
+            description = "Fetch all the products"
+    )
     @GetMapping
     public ResponseEntity<List<Product>> getAll() {
         return ResponseEntity.status(HttpStatus.OK)
@@ -27,6 +32,10 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
+    @Operation(
+            summary = "Get Product by ID",
+            description = "Fetch a single product by unique id"
+    )
     public ResponseEntity<Product> getById(@PathVariable Long id) {
 
         Product product = service.findById(id);
@@ -38,6 +47,10 @@ public class ProductController {
     }
 
     @PostMapping
+    @Operation(
+            summary = "Add a Product",
+            description = "Adds a new product to the database"
+    )
     public ResponseEntity<Product> create(@RequestBody Product product) {
         Product savedProduct = service.save(product);
         return ResponseEntity.status(HttpStatus.CREATED)
@@ -45,6 +58,10 @@ public class ProductController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Update a product",
+            description = "Updates the details of the product"
+    )
     public ResponseEntity<Product> update(@PathVariable Long id, @RequestBody Product product) {
         Product updatedProduct = service.update(id, product);
         if (updatedProduct == null) {
@@ -56,6 +73,10 @@ public class ProductController {
     }
 
     @DeleteMapping("/{id}")
+    @Operation(
+            summary = "Delete a Product",
+            description = "Deletes a product from the database by passing it's unique id"
+    )
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.delete(id);
         return ResponseEntity.status(HttpStatus.NO_CONTENT)
