b121cd011c6c36ae8725f247b0a695aa6fd5a859
Manu Yadav <manuyadav2716@gmail.com>
2026-03-10 11:00:32 +0100

    Add TODO for logging implementation in ProductController
    Added a TODO comment to implement logging in ProductController.

--- src/main/java/com/example/controller/ProductController.java
+++ src/main/java/com/example/controller/ProductController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//TODO: Implement Logging
 @RestController
 @RequestMapping("/products")
 public class ProductController {
